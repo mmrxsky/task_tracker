ce59b164c09f343b068d4ccb16ee4f4a31d1b385
miraxsky <mmrxsky@gmail.com>
2025-01-07 00:38:54 +0300

    add поля для модели задач

--- tracker/models.py
+++ tracker/models.py
@@ -1,13 +1,26 @@
 from django.db import models
 
-from users.models import NULLABLE
+from users.models import NULLABLE, User
 
 
 class Employee(models.Model):
     """Модель сотрудника"""
 
-    full_name = models.CharField(max_length=200, **NULLABLE, verbose_name='ФИО', help_text='Введите Фамилию, имя и отчество')
-    position = models.CharField(max_length=250, **NULLABLE, verbose_name='Должность', help_text='Укажите должность работника')
+    user = models.ForeignKey(
+        User, **NULLABLE, on_delete=models.CASCADE, verbose_name="Сотрудник"
+    )
+    full_name = models.CharField(
+        max_length=200,
+        **NULLABLE,
+        verbose_name="ФИО",
+        help_text="Введите Фамилию, имя и отчество",
+    )
+    position = models.CharField(
+        max_length=250,
+        **NULLABLE,
+        verbose_name="Должность",
+        help_text="Укажите должность работника",
+    )
 
     def __str__(self):
         return f'{self.full_name} - {self.position}'
@@ -19,14 +32,38 @@ class Employee(models.Model):
 class Task(models.Model):
     """Модель задачи"""
 
-    title = models.CharField(max_length=250, verbose_name='Название задачи')
-    description = models.TextField(**NULLABLE, verbose_name='Описание задачи')
-    executor = models.ForeignKey(Employee, on_delete=models.CASCADE, **NULLABLE, related_name='tasks', verbose_name='исполнитель задачи')
-    status = models.CharField(max_length=50, **NULLABLE, verbose_name='Статус')
-    time_complete = models.DateTimeField(**NULLABLE, verbose_name='Дата и время выполнения')
+    title = models.CharField(max_length=250, verbose_name="Название задачи")
+    description = models.TextField(**NULLABLE, verbose_name="Описание задачи")
+    executor = models.ForeignKey(
+        Employee,
+        on_delete=models.CASCADE,
+        **NULLABLE,
+        related_name="tasks",
+        verbose_name="Исполнитель задачи",
+    )
+    status = models.CharField(max_length=50, **NULLABLE, verbose_name="Статус")
+    time_complete = models.DateTimeField(
+        **NULLABLE, verbose_name="Дата и время выполнения"
+    )
+    parent_task = models.ForeignKey(
+        "self",
+        on_delete=models.SET_NULL,
+        **NULLABLE,
+        related_name="parent",
+        verbose_name="Родительская задача",
+    )
+    is_active = models.BooleanField(
+        default=False, verbose_name="Признак активной задачи"
+    )
+    is_related = models.BooleanField(
+        default=False, verbose_name="Признак связанной задачи"
+    )
 
     def __str__(self):
-        return f'{self.title}  {self.executor}'
+        return (
+            f"{self.title} {self.parent_task} {self.executor}"
+            f"{self.time_complete} {self.status} {self.is_related} {self.is_active}"
+        )
 
     class Meta:
         verbose_name = 'Задача'
