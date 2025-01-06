018a3b9c22387eafb968c1881f07e20e27d202a6
miraxsky <mmrxsky@gmail.com>
2025-01-07 00:24:09 +0300

    add приложение tracker

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('tracker.urls', namespace='tracker')),
     path('users/', include('users.urls', namespace='users')),
 ]
