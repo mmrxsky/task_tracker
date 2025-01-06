37c1427604cc3f76ef46f6064e6a4a43b327a81a
miraxsky <mmrxsky@gmail.com>
2025-01-07 00:27:42 +0300

    add CRUD модели сотрудника и задач

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 
-# Create your views here.
+from tracker.models import Employee, Task
+from tracker.serializers import EmployeeSerializer, TaskSerializer
+
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    """ViewSet для сотрудников"""
+    serializer_class = EmployeeSerializer
+    queryset = Employee.objects.all()
+
+
+class TaskListAPIView(ListAPIView):
+    """Класс для просмотра списка всех задач"""
+
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+
+
+class TaskRetrieveAPIView(RetrieveAPIView):
+    """Класс для просмотра конкретной задачи"""
+
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+
+
+class TaskCreateAPIView(CreateAPIView):
+    """Класс для создания новой задачи"""
+
+    serializer_class = TaskSerializer
+
+
+class TaskUpdateAPIView(UpdateAPIView):
+    """Класс для изменения конкретной задачи"""
+
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+
+
+class TaskDeleteAPIView(DestroyAPIView):
+    """Класс для удаления конкретной задачи"""
+
+    queryset = Task.objects.all()
