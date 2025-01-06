0889847511cc494d09af2e0f8840e2a1a1931da5
miraxsky <mmrxsky@gmail.com>
2025-01-07 01:18:02 +0300

    add Сериализатор для отслеживания выполненых задач

--- tracker/serializers.py
+++ tracker/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from tracker.models import Employee, Task
+from tracker.validators import RelatedTaskValid, NestingOfTaskValid, StatusTaskValid
 
 
 class EmployeeSerializer(serializers.ModelSerializer):
@@ -17,3 +18,27 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
+        validators = [
+            RelatedTaskValid(field='parent_task'),
+            NestingOfTaskValid(field='parent_task'),
+            StatusTaskValid(field='status'),
+        ]
+
+class EmployeeTrackSerializer(serializers.ModelSerializer):
+        """Сериализатор сотрудника с отслеживанием выполненных задач"""
+
+        active_task_count = serializers.SerializerMethodField()
+        tasks = TaskSerializer(many=True)
+
+        @staticmethod
+        def get_active_task_count(obj):
+            return obj.tasks.filter(is_active=True).count()
+
+        class Meta:
+            model = Employee
+            fields = ('full_name', 'position', 'active_task_count', 'tasks')
+            validators = [
+                RelatedTaskValid(field='parent_task'),
+                NestingOfTaskValid(field='parent_task'),
+                StatusTaskValid(field='status'),
+            ]
