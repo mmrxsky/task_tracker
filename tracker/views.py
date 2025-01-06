d5a61c3de84dea7d82cb7cf15bd9b084a595b5f4
miraxsky <mmrxsky@gmail.com>
2025-01-07 01:20:21 +0300

    add validation

--- tracker/views.py
+++ tracker/views.py
@@ -1,14 +1,31 @@
 from rest_framework import viewsets
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.permissions import IsAuthenticated
 
 from tracker.models import Employee, Task
 from tracker.serializers import EmployeeSerializer, TaskSerializer
+from users.permissions import IsStaff
 
 
 class EmployeeViewSet(viewsets.ModelViewSet):
     """ViewSet для сотрудников"""
     serializer_class = EmployeeSerializer
     queryset = Employee.objects.all()
+
+    def perform_create(self, serializer):
+        new_employee = serializer.save
+        new_employee.user = self.request.user
+        new_employee.save()
+
+    def get_permissions(self):
+        if self.action in ['create', 'destroy']:
+            self.permission_classes = (
+                IsAuthenticated,
+                IsStaff,
+            )
+        elif self.action in ['update', 'retrieve']:
+            self.permission_classes = (IsAuthenticated,)
+        return super().get_permissions()
 
 
 class TaskListAPIView(ListAPIView):
@@ -29,6 +46,12 @@ class TaskCreateAPIView(CreateAPIView):
     """Класс для создания новой задачи"""
 
     serializer_class = TaskSerializer
+    permission_classes = (IsAuthenticated, IsStaff)
+
+    def perform_create(self, serializer):
+        new_task = serializer.save()
+        new_task.user = self.request.user
+        new_task.save()
 
 
 class TaskUpdateAPIView(UpdateAPIView):
@@ -36,9 +59,11 @@ class TaskUpdateAPIView(UpdateAPIView):
 
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
+    permission_classes = (IsAuthenticated, IsStaff)
 
 
 class TaskDeleteAPIView(DestroyAPIView):
     """Класс для удаления конкретной задачи"""
 
     queryset = Task.objects.all()
+    permission_classes = (IsAuthenticated, IsStaff)
