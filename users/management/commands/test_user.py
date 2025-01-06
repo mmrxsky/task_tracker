84b8a804f7c8137fd5404f24005e22aef7d86da7
miraxsky <mmrxsky@gmail.com>
2025-01-07 01:42:53 +0300

    add тестового пользователя

---
+++ users/management/commands/test_user.py
@@ -0,0 +1,15 @@
+import os
+from django.core.management import BaseCommand
+from users.models import User
+
+
+class Command(BaseCommand):
+    """Команда для создания тестового пользователя в проекте"""
+
+    def handle(self, *args, **kwargs):
+        user = User.objects.create(
+            email='test@example.com',
+            is_active=True,
+        )
+        user.set_password('test')
+        user.save()
