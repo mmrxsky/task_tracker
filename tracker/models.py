521760922aecad8b1e1d78d0095c12a2e8a3cda6
miraxsky <mmrxsky@gmail.com>
2025-01-07 00:25:00 +0300

    add Модель сотрудника

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from users.models import NULLABLE
+
+
+class Employee(models.Model):
+    """Модель сотрудника"""
+
+    full_name = models.CharField(max_length=200, **NULLABLE, verbose_name='ФИО', help_text='Введите Фамилию, имя и отчество')
+    position = models.CharField(max_length=250, **NULLABLE, verbose_name='Должность', help_text='Укажите должность работника')
+
+    def __str__(self):
+        return f'{self.full_name} - {self.position}'
+
+    class Meta:
+        verbose_name = 'Сотрудник'
+        verbose_name_plural = 'Сотрудники'
