3515965ecb85a4d53a3c9d67fba927157e5956ee
miraxsky <mmrxsky@gmail.com>
2025-01-06 23:40:29 +0300

    add Описана модель пользователей

--- users/models.py
+++ users/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+NULLABLE = {"null": True, "blank": True}
+
+
+class User(AbstractUser):
+    """Модель пользователя"""
+
+    username = None
+    email = models.EmailField(
+        unique=True, verbose_name="Почта", help_text="Введите почту"
+    )
+    phone = models.CharField(
+        max_length=20,
+        **NULLABLE,
+        verbose_name="Номер телефона",
+        help_text="Введите номер телефона",
+    )
+    city = models.CharField(
+        max_length=100, **NULLABLE, verbose_name="Город", help_text="Введите город"
+    )
+    avatar = models.ImageField(
+        upload_to="users/",
+        **NULLABLE,
+        verbose_name="Аватар",
+        help_text="Загрузите аватар",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f"{self.email}"
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
