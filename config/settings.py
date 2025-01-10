b5a34ee105d4216c80ac4cf21ccca4346b456e96
miraxsky <mmrxsky@gmail.com>
2025-01-10 15:19:15 +0300

    fix cors

--- config/settings.py
+++ config/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt",
-    # "corsheaders",
+    "corsheaders",
     "drf_yasg",
     "django_filters",
     "users",
